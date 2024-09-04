351dab62306df1b932a93dc183ed037e44a178e2
rendizi <baglanov.a0930@gmail.com>
2024-09-04 22:23:57 +0500

    Export to google calendar

--- src/routes/events/events-router.ts
+++ src/routes/events/events-router.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+import NodeCache from 'node-cache';
 import calendarService from '../../services/calendarService';
+import { Subject } from '../../scrapper/types/types';
 
 const router = express.Router();
+const cache = new NodeCache({ stdTTL: 300 }); 
 
 router.get('/create-event', async (req, res) => {
   if (!req.isAuthenticated()) {
@@ -11,12 +15,44 @@ router.get('/create-event', async (req, res) => {
   const user = req.user as { profile: any; accessToken: string };
 
   try {
-    const event = await calendarService.createEvent(user.accessToken);
-    res.json({ message: 'Event created', event });
+    const uniqueId = uuidv4(); 
+
+    cache.set(uniqueId, { accessToken: user.accessToken });
+
+    res.redirect(`https://google.com?event_id=${uniqueId}`)
   } catch (error) {
-    console.error(error)
+    console.error(error);
     res.status(500).send('Error creating event');
   }
+});
+
+router.post('/save-event', async (req, res) => {
+  const id = req.headers['x-event-id'] as string 
+  if (!id) {
+    return res.status(400).send('Event ID is required');
+  }
+
+  const cachedData = cache.get<{ accessToken: string }>(id);
+
+  if (!cachedData) {
+    return res.status(404).send('Google session is exprired, try again');
+  }
+
+  const events: Subject[] = req.body;
+
+  if (!events || events.length === 0) {
+    return res.status(404).send('Event not found or expired');
+  }
+
+  try {
+    for (const event of events) {
+      await calendarService.createEvent(cachedData.accessToken, event); 
+    }
+      res.json({ message: 'Event saved successfully'});
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error saving event');
+    }
 });
 
 export default router;
--- src/services/calendarService.ts
+++ src/services/calendarService.ts
@@ -1,24 +1,46 @@
 import { google } from 'googleapis';
+import { addDays, formatISO, getDay, nextDay, setHours, setMinutes } from 'date-fns';
+import { Subject } from '../scrapper/types/types';
 
 const calendar = google.calendar('v3');
 
-async function createEvent(accessToken: string) {
+async function createEvent(accessToken: string, event: Subject) {
   const oauth2Client = new google.auth.OAuth2();
   oauth2Client.setCredentials({ access_token: accessToken });
+  event.day_of_week--
+
+  const now = new Date();
+  const currentDayOfWeek = getDay(now); 
+  
+  let eventDate: Date;
+  if (currentDayOfWeek === event.day_of_week) {
+    eventDate = now;
+  } else {
+    eventDate = nextDay(now, event.day_of_week as 0 | 1 | 2 | 3 | 4 | 5 | 6);
+  }
+  const [start_hour, start_min] = event.start_time.split(":").map(Number); 
+  const [end_hour, end_min] = event.end_time.split(":").map(Number);
+
+  const eventStart = formatISO(setMinutes(setHours(eventDate, start_hour), start_min), { format: 'extended' });
+  const eventEnd = formatISO(setMinutes(setHours(eventDate, end_hour), end_min), { format: 'extended' });
+
 
   try {
     const response = await calendar.events.insert({
       auth: oauth2Client,
-      calendarId: 'primary', 
+      calendarId: 'primary',
       requestBody: {
-        summary: 'Sample Event',
-        description: 'This is a sample event created by our application.',
+        summary: event.name,
+        description: `${event.name} with ${event.teacher} in ${event.cabinet}`,
         start: {
-          dateTime: '2024-09-01T10:00:00-07:00', 
+          dateTime: `${eventDate.toISOString().split('T')[0]}T${start_hour.toString().padStart(2, '0')}:${start_min.toString().padStart(2, '0')}:00+05:00`, 
+          timeZone: 'GMT+5',
         },
         end: {
-          dateTime: '2024-09-01T10:30:00-07:00', 
+          dateTime: `${eventDate.toISOString().split('T')[0]}T${end_hour.toString().padStart(2, '0')}:${end_min.toString().padStart(2, '0')}:00+05:00`, 
+          timeZone: 'GMT+5',
         },
+        recurrence: ['RRULE:FREQ=WEEKLY;BYDAY=' + ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'][event.day_of_week]],
       },
     });
 
